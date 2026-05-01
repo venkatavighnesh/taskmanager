948430fcca4297f447138e0b86fde74ac77ed4c1
kirankumar maddukuri <kirankumarmaddukuri8@gmail.com>
2026-05-01 19:54:03 +0530

    deploy: allow all hosts in vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: 'all',
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
